fa11cd7b323302f27ec17ddf8d5f365968f09fdb
mich5c18 <43537316+mich5c18@users.noreply.github.com>
2021-05-27 21:13:34 +0200

    Add files via upload

---
+++ VitecOpgave/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VitecOpgave
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly TextAnalyzer textAnalyzer = new TextAnalyzer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAnalyzeText_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtField.Text))
+            {
+                List<string> list = textAnalyzer.TextToSubstring(txtField.Text);
+                Dictionary<string, int> dictionary = textAnalyzer.SubstringFrequency(list);
+                List<KeyValuePair<string, int>> keyValueList = textAnalyzer.DictonarySorter(dictionary);
+
+                foreach (var pair in keyValueList)
+                {
+                    lstResult.Items.Add(pair);
+                }
+            }
+        }
+    }
+}
---
+++ VitecOpgave/TextAnalyzer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VitecOpgave
+{
+    //Jeg tænkte at det var en god ide at lave en klasse til at holde convertering og sorterings logik.
+    public class TextAnalyzer
+    {
+        /// <summary>
+        /// Converts a string in to it's substrings seperated by a whitespace " "
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>A List of strings</returns>
+        public List<string> TextToSubstring(string text)
+        {
+            string processedText = text.ToLower().Replace(".", "").Replace(",", "");
+
+            List<string> substringList = processedText.Split(" ").ToList();
+            
+            return substringList;
+        }
+
+        /// <summary>
+        /// Finds how many times strings exists in a List
+        /// </summary>
+        /// <param name="stringList"></param>
+        /// <returns>A Dictionary with a key value pair of <string, int></returns>
+        public Dictionary<string, int> SubstringFrequency(List<string> stringList)
+        {
+            Dictionary<string, int> frequencyDictonary = stringList.GroupBy(x => x)
+                                                                    .Where(g => g.Count() > 0)
+                                                                    .ToDictionary(x => x.Key, x => x.Count());
+
+            return frequencyDictonary;
+        }
+
+        /// <summary>
+        /// Sorts a Dictonary in descending order
+        /// </summary>
+        /// <param name="dictonary"></param>
+        /// <returns>A List of key value pairs</returns>
+        public List<KeyValuePair<string, int>> DictonarySorter(Dictionary<string, int> dictonary)
+        {
+            var items = from pair in dictonary 
+                        orderby pair.Value descending
+                        select pair;
+
+            List<KeyValuePair<string, int>> list = items.ToList();
+
+            return list;
+        }
+    }
+}
